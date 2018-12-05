b74bcd733b85e2335c4b26ac6aae950a6b33a18b
LolaAbudu <lolaabudu1589@gmail.com>
2018-12-04 21:56:16 -0500

    Lola-initial commit

---
+++ app/src/main/java/com/example/lolaabudu/group_portfolio_hw_team_coding_heroes/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.lolaabudu.group_portfolio_hw_team_coding_heroes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
