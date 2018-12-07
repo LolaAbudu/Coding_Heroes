a326e0401289646e566c955a5dd016b07837c07b
fribelguzman <fr33d00m@MacBook-Air-4.home>
2018-12-06 22:21:50 -0500

    finished drawer menu and main activity

--- app/src/main/java/com/example/lolaabudu/group_portfolio_hw_team_coding_heroes/MainActivity.java
+++ app/src/main/java/com/example/lolaabudu/group_portfolio_hw_team_coding_heroes/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lolaabudu.group_portfolio_hw_team_coding_heroes;
 
 import android.content.Intent;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -45,9 +46,17 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
 
+    }
 }
-
 
 
 
