72ba71a18de09340b0d76558b1586bacd9734af8
rosannaNaomy <naomypursuit.org>
2018-12-07 20:43:03 -0500

    updated menu and strings xml

--- app/src/main/java/com/example/lolaabudu/group_portfolio_hw_team_coding_heroes/NaomyProfileActivity.java
+++ app/src/main/java/com/example/lolaabudu/group_portfolio_hw_team_coding_heroes/NaomyProfileActivity.java
@@ -1,7 +1,16 @@
 package com.example.lolaabudu.group_portfolio_hw_team_coding_heroes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.Toolbar;
 
 public class NaomyProfileActivity extends AppCompatActivity {
 
@@ -12,6 +21,9 @@ public class NaomyProfileActivity extends AppCompatActivity {
 
 
     }
+
+
+
 
 
 }
